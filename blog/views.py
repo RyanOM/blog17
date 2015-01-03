3d6cd743976dbd60e8b54d386e613f68b1ff9f78
Ryan O'Mullan <omullan.ryan@gmail.com>
2015-01-03 14:36:46 +0100

    Pagination + disqus

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,32 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+from taggit.models import Tag
+
 from .models import Post
 
+#local functions
+def _paginate_posts(request, posts):
+	paginator = Paginator(posts, 3)
+	#providing page with current
+	page = request.GET.get('page', 1)
+	try:
+		posts = paginator.page(page)
+	except PageNotAnInteger:
+		posts = paginator.page(1)
+	except EmptyPage:
+		posts = paginator.page(paginator.num_pages)
+	return posts
 
 # Create your views here.
 def post_list(request):
 	posts = Post.objects.filter(published_date__isnull=False).order_by('-published_date')
 	#return render(request, 'blog/post_list.html', {'posts': posts})
-	return render(request, 'blog/post_list.html', {'posts': posts})
+	paginated = _paginate_posts(request, posts)
+	return render(request, 'blog/post_list.html', {
+		'posts': paginated,
+		'tags': Tag.objects.all(),
+	})
 
 def post_detail(request, slug=""):
 	try:
@@ -21,4 +41,9 @@ def posts_by_tag(request, slug=""):
 	posts = Post.objects.filter(tags__slug=slug.lower())
 	#if posts.count() == 0:
 	#	raise Exception("whatever")
-	return render(request, 'blog/post_list.html', {'posts': posts, 'slug': slug})
+	paginated = _paginate_posts(request, posts)
+	return render(request, 'blog/post_list.html', {
+		'posts': paginated,
+		'slug': slug,
+		'tags': Tag.objects.all(),
+	})
