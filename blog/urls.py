9e11c7ebfee4f35d63f8c186cb7601393293a39c
Ryan O'Mullan <omullan.ryan@gmail.com>
2015-01-03 20:22:11 +0100

    Fixed urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = patterns('',
 		url(r'^$', views.post_list),
 
-		url(r'^/tag/(?P<slug>[^\.]+)$', views.posts_by_tag, name='blog_posts_by_tag'),
+		url(r'^tag/(?P<slug>[^\.]+)$', views.posts_by_tag, name='blog_posts_by_tag'),
 		
-		url(r'^/(?P<slug>[^\.]+)$', views.post_detail, name='blog_post_view_detail'),
+		url(r'^(?P<slug>[^\.]+)$', views.post_detail, name='blog_post_view_detail'),
 
 )
